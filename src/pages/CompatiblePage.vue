3b3c175304d475ea8fe90a841778b7bcde98e984
rusberry <seiten4@gmail.com>
2024-06-10 22:50:51 +0300

    fix compatiblepage margin

--- src/pages/CompatiblePage.vue
+++ src/pages/CompatiblePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto py-8">
+  <div class="">
     <h1 class="text-2xl font-bold mb-4">Сравнение программного обеспечения</h1>
     <div class="overflow-x-auto">
       <table class="min-w-full bg-white">
